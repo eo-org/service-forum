11910afd2d0125efc00efe1a3e08b1376182227a
wangliangfeng <Administrator@HE7OBPBFRPQU9YF.(none)>
2012-05-16 09:41:14 +0800

    js

--- html/script/restpost.js
+++ html/script/restpost.js
@@ -1,5 +1,5 @@
-﻿var httpurl = "http://forum.eo.test/" ;
-//var httpurl = "http://forum.enorange.cn/" ;
+﻿//var httpurl = "http://forum.eo.test/" ;
+var httpurl = "http://forum.enorange.cn/" ;
 
 $(document).ready(function() { 
 	var obj = $('#service-forum-content').attr('org-code');
